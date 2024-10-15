3fcb0ee2150e34ad940c552d5d78a951dbf70842
blazete <shreyansh7309992028@gmail.com>
2024-10-15 15:20:30 +0530

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import TextEditor from './TextEditor';
+import React from "react"
+import TextEditor from "./TextEditor"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom"
+import { v4 as uuidV4 } from "uuid"
+
+const Home = () => <Navigate to={`/documents/${uuidV4()}`} replace />
 
 function App() {
   return (
-    <>
-      <TextEditor />
-    </>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './style.css'; // Add this line to import the CSS
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App"
+import "./style.css"
 
-import App from './App';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
-
+  <React.StrictMode>
     <App />
-
-);
-
-
-
+  </React.StrictMode>
+)
